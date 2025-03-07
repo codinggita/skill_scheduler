b6ae252c26ac7f690a71ff8e5d64a2602d747588
Ishita Trivedi <ishita.trivedi.cg@gmail.com>
2025-03-07 15:49:44 +0530

    added vercel.json

--- frontendNew/src/App.jsx
+++ frontendNew/src/App.jsx
@@ -12,7 +12,6 @@ const App = () => {
   return (
     <Router>
       <Navbar/>
-      
       <Routes>
         <Route path="/" element={<Navigate to="/dashboard" />} />
         <Route path="/dashboard" element={<Dashboard />} />
